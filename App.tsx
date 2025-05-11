0c8689e2112a7b829722cc735ad93f2b81fa1054
Akhil Pillai <ackledotdev@gmail.com>
2025-05-11 14:57:22 -0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { HapticOptions, trigger as triggerHaptics } from "react-native-haptic-feedback";
+
+const HapticOpts: HapticOptions = {
+// The whole point is to vibrate
+	ignoreAndroidSystemSettings: true
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Pressable
+				hitSlop={10}
+				onPressIn={() => {
+					triggerHaptics('rigid', HapticOpts)
+				}}
+				style={({ pressed }) => [
+					{
+						backgroundColor: pressed ? 'rgba(255, 0, 0, 0.3)' : 'rgba(255, 0, 0, 0.1)',
+						padding: 20,
+						borderRadius: 10
+					}
+				]}
+			>
+				<Text>Rigid</Text>
+			</Pressable>
+			<StatusBar style='auto' />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		padding: 4,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
